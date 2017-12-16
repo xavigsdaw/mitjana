cfd0517a123ebb1e4e85c6abe4793681fe5416d0
xavigsdaw <xavigsdaw@example.com>
2017-12-16 16:34:26 +0100

    Segon commit del projecte mitjana

--- mitjana.c
+++ mitjana.c
@@ -15,6 +15,7 @@ int main() {
 		printf("Vols continuar (s/n): ");
 		scanf("%c",&consulta); 
 	}
+
 	mitjana=suma/numero_repeticions;
 	printf("La mitjana resultant és: %.2f\n",mitjana);	
 	return 0;
